cd7e276d6ec57e203039ce6984f3193074775e63
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2017-01-01 18:21:42 +0100

    Now using min.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,36 @@
 // Less configuration
 var gulp = require('gulp');
 var less = require('gulp-less');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 
 gulp.task('less', function() {
-    gulp.src(['!node_modules/**/*.less', '**/[^_]*.less'])
-        .pipe(less())
-        .pipe(gulp.dest(function(f) {
-            return f.base;
+    // All .less files; excluding node_modules folder and partials
+    var targets = ['!node_modules/**/*.*', '**/[^_]*.less'];
+
+    // Generate css
+    gulp.src(targets)
+        .pipe(less().on('error', function(error) {
+            console.error(error)
         }))
+        .pipe(gulp.dest(function(f) {
+            console.log("Generated " + f.path);
+            return f.base;
+        }));
+
+    // Generate min.css
+    gulp.src(targets)
+        .pipe(less().on('error', function(error) {
+            console.error(error)
+        }))
+        .pipe(cssmin().on('error', function(error) {
+            console.error(error);
+        }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(function(f) {
+            console.log("Generated " + f.path);
+            return f.base;
+        }));
 });
 
 gulp.task('default', ['less'], function() {
